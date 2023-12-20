3c8067a3d9843bdd57acea4d259f53e583d1069e
AfgMS <127741557+AfgMS@users.noreply.github.com>
2023-12-20 09:47:13 +0700

    Update SigmaDev.lua

--- SigmaDev.lua
+++ SigmaDev.lua
@@ -244,13 +244,6 @@ function Library:createTabs(parentFrame, tabName)
 	TAB.Tabs.Name = "Tabs"
 	TAB.Tabs.Visible = false
 	
-	TAB.FakeBlur = Instance.new("Frame", parentFrame)
-	TAB.FakeBlur.Position = UDim2.new(0, 0, 0, 0)
-	TAB.FakeBlur.Size = UDim2.new(1, 0, 1, 0)
-	TAB.FakeBlur.BackgroundTransparency = 0.45
-	TAB.FakeBlur.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-	TAB.FakeBlur.Visible = false
-	
 	TAB.TabsName = Instance.new("TextLabel", TAB.Tabs)
 	TAB.TabsName.BorderSizePixel = 0
 	TAB.TabsName.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -298,7 +291,6 @@ function Library:createTabs(parentFrame, tabName)
 			if input.KeyCode == Enum.KeyCode.V and not gameProcessedEvent then
 				TAB.Tabs.Visible = not TAB.Tabs.Visible
 				TAB.ScrollingPart.Visible = not TAB.ScrollingPart.Visible
-				TAB.FakeBlur.Visible = not TAB.FakeBlur.Visible
 			end
 		end)
 	
