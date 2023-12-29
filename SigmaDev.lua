5d138feb13980c0771599524e6e6b52e1ccd2f70
AfgMS <127741557+AfgMS@users.noreply.github.com>
2023-12-29 12:20:47 +0700

    Update SigmaDev.lua

--- SigmaDev.lua
+++ SigmaDev.lua
@@ -701,9 +701,11 @@ ButtonsMenuTitle.ZIndex = 2
 					DropdownHolders.MouseButton1Click:Connect(function() 
 						DropdownMenu.Visible = not DropdownMenu.Visible
 					end)
+
 					return Dropdown
 				end
-					return ToggleButtonInsideUI
+
+				return ToggleButtonInsideUI
 			end
 
 			return Slider
@@ -714,6 +716,102 @@ ButtonsMenuTitle.ZIndex = 2
 
 	return TAB
 end
-	return Library
-end
 
+
+Library:createScreenGui()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Sigma
+createnotification("Sigma", "Welcome to Sigma, Press V", 1, true)
+
+local tab1 = Library:createTabs(CoreGui.Sigma, "Gui")
+local tab2 = Library:createTabs(CoreGui.Sigma, "Combat")
+
+local button1 = tab1:ToggleButton({
+	name = "Hendro",
+	info = "QuACK Quack",
+	callback = function(enabled)
+		print("cum")
+	end
+})
+local SliderStuff = button1:Slider({
+	title = "Dugong",
+	min = 5,
+	max = 10,
+	default = 5,
+	callback = function(val)
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+	end
+})
+local ToggleInsideUI1 = button1:ToggleButtonInsideUI({
+	name = "MyFirne",
+	callback = function(enabled)
+		if enabled then
+			print("hello")
+		end
+	end
+})
+local Dropdown = button1:Dropdown({
+	name = "Yes",
+	todo = "E",
+	list = {"Walk", "Run", "Sprint"},
+	Default = "Walk",
+	callback = function(selectedItem)
+		print("Movement type set to:", selectedItem)
+	end
+})
+local button99 = tab2:ToggleButton({
+	name = "UninjectShit",
+	info = "Click to uninject the Sigma hack.",
+	callback = function(enabled)
+		if enabled then
+			CoreGui.Sigma:Destroy()
+			print("Destroyed Main")
+			CoreGui.SigmaVisualStuff:Destroy()
+			print("Destroyed Notif")
+		end
+	end
+})
+local SliderStuff = button99:Slider({
+	title = "Walkspeed",
+	min = 10,
+	max = 200,
+	default = 5,
+	callback = function(val)
+		print("" ..val)
+	end
+})
+local ToggleInsideUI1 = button99:ToggleButtonInsideUI({
+	name = "MyFirne",
+	callback = function(enabled)
+		if enabled then
+			print("hello")
+		end
+	end
+})
+local Dropdown = button99:Dropdown({
+	name = "Yes",
+	todo = "E",
+	list = {"Walk", "Run", "Sprint"},
+	Default = "Walk",
+	callback = function(selectedItem)
+		print("Movement type set to:", selectedItem)
+	end
+})
