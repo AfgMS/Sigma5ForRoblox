56cee775836ba7c035292c858384c4038a0a8b45
AfgMS <127741557+AfgMS@users.noreply.github.com>
2024-02-04 13:20:01 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -4,19 +4,6 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local localPlayer = game.Players.LocalPlayer
 
---Remote
-local KewAuraUwU = ReplicatedStorage.rbxts_include.node_modules["@rbxts"].net.out._NetManaged.SwordHit
-
---Stuff
-local SwordInfo = {
-    [1] = { Name = "wood_sword", Display = "Wood Sword", Rank = 1 },
-    [2] = { Name = "stone_sword", Display = "Stone Sword", Rank = 2 },
-    [3] = { Name = "iron_sword", Display = "Iron Sword", Rank = 3 },
-    [4] = { Name = "diamond_sword", Display = "Diamond Sword", Rank = 4 },
-    [5] = { Name = "emerald_sword", Display = "Emerald Sword", Rank = 5 },
-    [6] = { Name = "rageblade", Display = "Rage Blade", Rank = 6 },
-}
-
 --Function
 local function LibraryCheck()
     local SigmaCheck = CoreGui:FindFirstChild("Sigma")
@@ -40,56 +27,33 @@ local function LibraryCheck()
     end
 end
 
-local function isAlive(localPlayer)
-    if not localPlayer or not localPlayer.Character or not localPlayer.Character:FindFirstChild("Head") or not localPlayer.Character:FindFirstChild("Humanoid") then
-        warn("Bruh")
-        return false
-    end
+-- local sliderStuff = ActiveMods:Slider({
+--     title = "ExampleSliders",
+--     min = 5,
+--     max = 10,
+--     default = 5,
+--     callback = function(value)
+--         print("current value rn is" .. value)
+--     end
+-- })
 
-    return true
-end
+-- local toggleInsideUI1 = ActiveMods:ToggleButtonInsideUI({
+--     name = "ExampleMiniToggle",
+--     callback = function(enabled)
+--         if enabled then
+--             print("hello")
+--         end
+--     end
+-- })
 
-local function findNearestLivingPlayer()
-    local nearestPlayer
-    local nearestDistance = math.huge
-
-    for _, player in ipairs(game.Players:GetPlayers()) do
-        if player ~= localPlayer and isAlive(player) then
-            local distance = (player.Character.HumanoidRootPart.Position - localPlayer.Character.HumanoidRootPart.Position).Magnitude
-            if distance < nearestDistance then
-                nearestPlayer = player
-                nearestDistance = distance
-            end
-        end
-    end
-end
-
-local function attackValue(vec)
-  return { value = vec }
-end
-
-local function getcloserpos(pos1, pos2, amount)
-  local newPos = (pos2 - pos1).Unit * math.min(amount, (pos2 - pos1).Magnitude) + pos1
-  return newPos
-end
-
-local function getBestSword()
-  local bestsword = nil
-  local bestrank = 0
-  for i, v in pairs(localPlayer.Character.InventoryFolder.Value:GetChildren()) do
-    if v.Name:match("sword") or v.Name:match("blade") then
-      for _, data in pairs(SwordInfo) do
-        if data["Name"] == v.Name then
-          if bestrank <= data["Rank"] then
-            bestrank = data["Rank"]
-            bestsword = v
-          end
-        end
-      end
-    end
-  end
-  return bestsword
-end
+-- local dropdown = ActiveMods:Dropdown({
+--     name = "ExampleDropdown",
+--     default = "Test",
+--     list = {"Walk", "Run", "Sprint"},
+--     callback = function(selectedItem)
+--         print("Movement type set to:", selectedItem)
+--     end
+-- })
 
 --SigmaUI
 Library:createScreenGui()
@@ -97,42 +61,31 @@ LibraryCheck()
 createnotification("Sigma", "Welcome to Sigma, Press V", 1, true)
 
 local tab1 = Library:createTabs(CoreGui.Sigma, "Gui")
-local tab2 = Library:createTabs(CoreGui.Sigma, "Combat")
 
 --ActiveMods
 local ActiveMods = tab1:ToggleButton({
     name = "ActiveMods",
-    info = "Arraylist goes brrr",
+    info = "Render active mods",
     callback = function(enabled)
             CoreGui.SigmaVisualStuff.ArrayListHolder.Visible = not CoreGui.SigmaVisualStuff.ArrayListHolder.Visible
     end
 })
-local sliderStuff = ActiveMods:Slider({
-    title = "Dugong",
-    min = 5,
-    max = 10,
-    default = 5,
-    callback = function(value)
-        print("current value rn is" .. value)
-    end
-})
-local toggleInsideUI1 = ActiveMods:ToggleButtonInsideUI({
-    name = "MyFirne",
+--TabGUI
+local TabGUI = tab1:ToggleButton({
+    name = "TabGUI",
+    info = "Just decorations",
     callback = function(enabled)
-        if enabled then
-            print("hello")
-        end
+            CoreGui.SigmaVisualStuff.TabHolder.Visible = not CoreGui.SigmaVisualStuff.TabHolder.Visible
     end
 })
-local dropdown = ActiveMods:Dropdown({
-    name = "Testing",
-    default = "Test",
-    list = {"Walk", "Run", "Sprint"},
-    callback = function(selectedItem)
-        print("Movement type set to:", selectedItem)
+--Cords
+local Cords = tab1:ToggleButton({
+    name = "Cords",
+    info = "Display positions",
+    callback = function(enabled)
+            CoreGui.SigmaVisualStuff.Cordinate.Visible = not CoreGui.SigmaVisualStuff.Cordinate.Visible
     end
 })
-
 --Uninject
 local Uninject = tab1:ToggleButton({
     name = "UninjectTest",
@@ -143,45 +96,6 @@ local Uninject = tab1:ToggleButton({
             print("Destroyed Main")
             CoreGui.SigmaVisualStuff:Destroy()
             print("Destroyed Notif")
-        end
-    end
-})
-    
---KillAura
-local AttackDelay
-local target = findNearestLivingPlayer(20)
-local cam = game.Workspace.CurrentCamera
-local mouse = Ray.new(cam.CFrame.Position, target.Character.HumanoidRootPart.Position).Unit.Direction
-
-local KillAura = tab2:ToggleButton({
-    name = "KillAura",
-    info = "KillAura Testing",
-    callback = function(enabled)
-        if enabled then
-            if isAlive(localPlayer) then
-                AttackDelay = 0.03
-                if target and target.Character then
-                    while wait(AttackDelay) do
-                        KewAuraUwU:FireServer({
-                            ["entityInstance"] = target.Character,
-                            ["chargedAttack"] = {
-                                ["chargeRatio"] = 1
-                            },
-                            ["validate"] = {
-                                ["raycast"] = {
-                                    ["cursorDirection"] = attackValue(mouse),
-                                    ["cameraPosition"] = attackValue(target.Character.HumanoidRootPart.Position),
-                                },
-                                ["selfPosition"] = attackValue(getcloserpos(localPlayer.Character.HumanoidRootPart.Position, target.Character.HumanoidRootPart.Position, 2)),
-                                ["targetPosition"] = attackValue(target.Character.HumanoidRootPart.Position),
-                            },
-                            ["weapon"] = getBestSword()
-                        })
-                    end
-                else
-                    AttackDelay = 86400
-                end
-            end
         end
     end
 })
