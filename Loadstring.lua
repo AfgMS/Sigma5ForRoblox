797d24954badf7d26513387bcbf4b36916e51142
AfgMS <127741557+AfgMS@users.noreply.github.com>
2024-02-10 12:00:33 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,6 +1,8 @@
 if game:IsLoaded() then
     if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/Bedwars.lua"))()
+    elseif game.PlaceId == 8542275097 or game.PlaceId == 8768229691 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/Skywars.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/Universal.lua"))()
     end
