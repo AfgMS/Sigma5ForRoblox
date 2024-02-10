3c57ae1fcbcc2cae5635696e861d8bc5d0a8ac72
AfgMS <127741557+AfgMS@users.noreply.github.com>
2024-02-10 11:58:56 +0700

    Create Skywars.lua

---
+++ Skywars.lua
@@ -0,0 +1,104 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/SigmaDev.lua", true))()
+local CoreGui = game:WaitForChild("CoreGui")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local localPlayer = game.Players.LocalPlayer
+
+--Function
+local function LibraryCheck()
+    local SigmaCheck = CoreGui:FindFirstChild("Sigma")
+    local SigmaVisualCheck = CoreGui:FindFirstChild("SigmaVisualStuff")
+    
+    if not SigmaCheck then
+        print("Error: Sigma ScreenGui not found in CoreGui.")
+    elseif SigmaCheck then
+        print("Debug: SigmaCheck Found")
+    elseif not SigmaVisualCheck then
+        print("Error: SigmaVisualStuff ScreenGui not found in CoreGui.")
+    elseif SigmaVisualCheck then
+        print("Debug: SigmaVisualCheck Found")
+        local ArraylistCheck = SigmaVisualCheck:FindFirstChild("ArrayListHolder")
+        if not ArraylistCheck then
+            print("Error: ArrayList Holder not found in SigmaVisualStuff.")
+        elseif ArraylistCheck then
+            print("Debug: ArrayList Found")
+            return
+        end
+    end
+end
+local function getNearestPlayer(range)
+    local nearestPlayer
+    local nearestDistance = math.huge
+
+    for _, player in ipairs(Players:GetPlayers()) do
+        if player.Character and player.Character.PrimaryPart then
+            local playerPosition = player.Character.PrimaryPart.Position
+            local distance = (playerPosition - game.Workspace.CurrentCamera.CFrame.Position).magnitude
+            if distance < nearestDistance then
+                nearestPlayer = player
+                nearestDistance = distance
+            end
+        end
+    end
+
+    return nearestPlayer
+end
+--SigmaUI
+Library:createScreenGui()
+LibraryCheck()
+createnotification("Sigma5", "Loaded Successfully", 1, true)
+
+local GUItab = Library:createTabs(CoreGui.Sigma, "Gui")
+--ActiveMods
+local ActiveMods = GUItab:ToggleButton({
+    name = "ActiveMods",
+    info = "Render active mods",
+    callback = function(enabled)
+            CoreGui.SigmaVisualStuff.ArrayListHolder.Visible = not CoreGui.SigmaVisualStuff.ArrayListHolder.Visible
+    end
+})
+--TabGUI
+local TabGUI = GUItab:ToggleButton({
+    name = "TabGUI",
+    info = "Just decorations",
+    callback = function(enabled)
+            CoreGui.SigmaVisualStuff.LeftHolder.TabHolder.Visible = not CoreGui.SigmaVisualStuff.LeftHolder.TabHolder.Visible
+    end
+})
+--Uninject
+local Uninject = GUItab:ToggleButton({
+    name = "DeleteGUI",
+    info = "Doesnt Uninject 100%",
+    callback = function(enabled)
+        if enabled then
+            CoreGui.Sigma:Destroy()
+            print("Destroyed Main")
+            CoreGui.SigmaVisualStuff:Destroy()
+            print("Destroyed Notif")
+        end
+    end
+})
+--CombatSection
+local COMBATtab = Library:createTabs(CoreGui.Sigma, "Combat")
+--KillAura
+local Delay = 0.03
+local KillAura = COMBATtab:ToggleButton({
+    name = "KillAura",
+    info = "Attack Nearest Player?",
+    callback = function(enabled)
+        if enabled then
+            if localPlayer and localPlayer.Character then
+                local NearestPlayer = getNearestPlayer(20)
+                if NearestPlayer and NearestPlayer.Character then
+                    while wait(Delay) do
+                        local args = {
+                            [1] = NearestPlayer.Character.HumanoidRootPart
+                        }
+
+                        game:GetService("ReplicatedStorage"):FindFirstChild("events-Eqz"):FindFirstChild("5c73e2ee-c179-4b60-8be7-ef8e4a7eebaa"):FireServer(unpack(args))
+                    end
+                end
+            end
+        end
+    end
+})
