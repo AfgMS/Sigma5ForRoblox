8fcf8009ef48a9e229d5d1fea1ccde34d8c22ae1
AfgMS <127741557+AfgMS@users.noreply.github.com>
2024-02-11 22:21:19 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -2,31 +2,33 @@ local SkywarsGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Sky
 local BedwarsGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Bedwars.lua"
 local UniversalGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Universal.lua"
 
+local function GithubUpdateCheck(url, filePath)
+    local content = game:HttpGet(url)
+    if content then
+        writefile(filePath, content)
+        print("Downloaded " .. filePath .. " from GitHub.")
+    else
+        warn("Failed to download " .. filePath .. " from GitHub.")
+    end
+end
+
 if not isfolder("Sigma5") then
     makefolder("Sigma5")
 end
 
-if not isfile("Sigma5/Universal.lua") then
-    local UniversalSource = game:HttpGet(UniversalGithub)
-    writefile("Sigma5/Universal.lua", UniversalSource)
-end
-
-if not isfile("Sigma5/Skywars.lua") then
-    local SkywarsSource = game:HttpGet(SkywarsGithub)
-    writefile("Sigma5/Skywars.lua", SkywarsSource)
-end
-
-if not isfile("Sigma5/Bedwars.lua") then
-    local BedwarsSource = game:HttpGet(BedwarsGithub)
-    writefile("Sigma5/Bedwars.lua", BedwarsSource)
-end
+GithubUpdateCheck(UniversalGithub, "Sigma5/Universal.lua")
+GithubUpdateCheck(SkywarsGithub, "Sigma5/Skywars.lua")
+GithubUpdateCheck(BedwarsGithub, "Sigma5/Bedwars.lua")
 
 wait(1)
-
+print("Debug Logs")
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
+    print("Loaded Bedwars.lua")
     loadfile("Sigma5/Bedwars.lua")()
 elseif game.PlaceId == 8542275097 or game.PlaceId == 8768229691 then
+    print("Loaded Skywars.lua...")
     loadfile("Sigma5/Skywars.lua")()
 else
+    print("Loaded Universal.lua...")
     loadfile("Sigma5/Universal.lua")()
 end
