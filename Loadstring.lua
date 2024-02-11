776f07857509087e74824ef7f2336c254779be03
AfgMS <127741557+AfgMS@users.noreply.github.com>
2024-02-11 22:08:01 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,9 +1,32 @@
-if game:IsLoaded() then
-    if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/Bedwars.lua"))()
-    elseif game.PlaceId == 8542275097 or game.PlaceId == 8768229691 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/Skywars.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Simga345/main/Universal.lua"))()
-    end
+local SkywarsGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Skywars.lua"
+local BedwarsGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Bedwars.lua"
+local UniversalGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Universal.lua"
+
+if not isfolder("Sigma5") then
+    makefolder("Sigma5")
+end
+
+if not isfile("Sigma5/Universal.lua") then
+    local UniversalSource = game:HttpGet(UniversalGithub)
+    writefile("Sigma5/Universal.lua", UniversalSource)
+end
+
+if not isfile("Sigma5/Skywars.lua") then
+    local SkywarsSource = game:HttpGet(SkywarsGithub)
+    writefile("Sigma5/Skywars.lua", SkywarsSource)
+end
+
+if not isfile("Sigma5/Bedwars.lua") then
+    local BedwarsSource = game:HttpGet(BedwarsGithub)
+    writefile("Sigma5/Bedwars.lua", BedwarsSource)
+end
+
+wait(1)
+
+if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
+    loadfile("Sigma5/Bedwars.lua")()
+elseif game.PlaceId == 8542275097 or game.PlaceId == 8768229691 then
+    loadfile("Sigma5/Skywars.lua")()
+else
+    loadfile("Sigma5/Universal.lua")()
 end
