991b6c390ea19209083086291157518b6f8bdaca
AfgMS <127741557+AfgMS@users.noreply.github.com>
2024-02-18 00:14:59 +0700

    Update Bedwars.lua

--- Bedwars.lua
+++ Bedwars.lua
@@ -6,6 +6,7 @@ local localPlayer = game.Players.LocalPlayer
 local Camera = game:GetService("Workspace").CurrentCamera
 local UserInputService = game:GetService("UserInputService")
 local Lighting = game:GetService("Lighting")
+local lplrname = localPlayer.Name
 --Functions
 local function LibraryCheck()
     local SigmaCheck = CoreGui:FindFirstChild("Sigma")
@@ -48,9 +49,6 @@ local function GetNearestPlr(range)
 
     return nearestPlayer
 end
-local function Value2Vector(vec)
-  return { value = vec }
-end
 local WeaponRank = {
   [1] = { Name = "wood_sword", Rank = 1 },
   [2] = { Name = "stone_sword", Rank = 2 },
@@ -60,9 +58,47 @@ local WeaponRank = {
   [6] = { Name = "emerald_sword", Rank = 6 },
   [7] = { Name = "rageblade", Rank = 7 },
 }
+local ProjectilesRank = {
+  [1] = { Name = "wood_bow", Rank = 1 },
+  [2] = { Name = "fireball", Rank = 2 },
+  [3] = { Name = "wood_crossbow", Rank = 3 },
+  [4] = { Name = "firecrackers", Rank = 4 },
+  [5] = { Name = "headhunter", Rank = 5 }
+}
+local function Value2Vector(vec)
+  return { value = vec }
+end
 function GetAttackPos(plrpos, nearpost, val)
   local newPos = (nearpost - plrpos).Unit * math.min(val, (nearpost - plrpos).Magnitude) + plrpos
   return newPos
+end
+local function ChangeHotbar(item)
+    if localPlayer.Character.HandInvItem.Value ~= item then
+        local inventoryFolder = localPlayer.Character.InventoryFolder.Value
+        for _, child in ipairs(inventoryFolder:GetChildren()) do
+            if child.Name == item then
+                game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("SetInvItem"):InvokeServer({ hand = child })
+                break
+            end
+        end
+    end
+end
+local function GetProjectiles()
+  local bestProject = nil
+  local bestrank = 0
+  for i, v in pairs(localPlayer.Character.InventoryFolder.Value:GetChildren()) do
+    if v.Name:match("bow") or v.Name:match("fire") or v.Name:match("head") then
+      for _, data in pairs(ProjectilesRank) do
+        if data["Name"] == v.Name then
+          if bestrank <= data["Rank"] then
+            bestrank = data["Rank"]
+            bestProject = v
+          end
+        end
+      end
+    end
+  end
+  return bestProject
 end
 local function GetSword()
   local bestsword = nil
@@ -203,7 +239,41 @@ local CustomLowHealth = AutoRageQuit:Slider({
         LowHealthValue = value
     end
 })
---BowAimbot Coming Soon
+--[[
+local BowAimbotDelay
+local BowAimbot = CombatTab:ToggleButton({
+    name = "BowAimbot",
+    info = "Vape ProjectileExploit??",
+    callback = function(enabled)
+        if enabled then
+            BowAimbotDelay = 1
+            local NearestPlayer = GetNearestPlr(math.huge)
+            if NearestPlayer and not NearestPlayer:FindFirstChildOfClass("ForceField") then
+                local ProjectileWeapon = ChangeHotbar(GetProjectiles())
+                while wait(BowAimbotDelay) do
+                    local BowAimbotRequirement = {
+                        [1] = ProjectileWeapon,
+                        [2] = "arrow",
+                        [3] = ProjectileWeapon,
+                        [4] = Value2Vector(NearestPlayer.Character:FindFirstChild("HumanoidRootPart").Position),
+                        [5] = Value2Vector(NearestPlayer.Character:FindFirstChild("HumanoidRootPart").Position),
+                        [6] = Vector3.new(0, NearestPlayer.Character:FindFirstChild("HumanoidRootPart").Position.Y, 0),
+                        [7] = HttpService:GenerateGUID(true),
+                        [8] = {
+                            ["drawDurationSeconds"] = 0.95,
+                            ["shotId"] = HttpService:GenerateGUID(false)
+                        },
+                        [9] = Workspace:GetServerTimeNow() - 0.11
+                    }
+                    game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged.ProjectileFire:InvokeServer(unpack(BowAimbotRequirement))
+                end
+            else
+                BowAimbotDelay = 86000
+            end
+        end
+    end
+})
+--]]
 --KillAura
 local KillAuraRange
 local RotationsRange
@@ -216,7 +286,7 @@ local KillAura = CombatTab:ToggleButton({
             KillAuraRange = 20
             while wait(0.01) do
                 local NearestPlayer = GetNearestPlr(KillAuraRange)
-                if NearestPlayer then
+                if NearestPlayer and not NearestPlayer:FindFirstChildOfClass("ForceField") then
                     KillAuraCriticalEffect = true
                     ReplicatedStorage.rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged.SwordHit:FireServer({
                         ["entityInstance"] = NearestPlayer.Character,
@@ -231,7 +301,7 @@ local KillAura = CombatTab:ToggleButton({
                             ["selfPosition"] = Value2Vector(GetAttackPos(localPlayer.Character:FindFirstChild("HumanoidRootPart").Position, NearestPlayer.Character:FindFirstChild("HumanoidRootPart").Position, 2)),
                             ["targetPosition"] = Value2Vector(NearestPlayer.Character.HumanoidRootPart.Position),
                         },
-                        ["weapon"] = GetSword()
+                        ["weapon"] = ChangeHotbar(GetSword())
                     })
                 end
             end
