f14260503d640102b1819ee210b42253ecf31e8c
NotHm_ <127741557+AfgMS@users.noreply.github.com>
2024-03-01 19:48:19 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,21 +1,28 @@
-local BedwarsGithub = "https://raw.githubusercontent.com/AfgMS/Simga345/main/Bedwars.lua"
+local SkywarsGithub = "https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/Skywars.lua"
+
 local function GithubUpdateCheck(url, filePath)
     local content = game:HttpGet(url)
     if content then
-        writefile(filePath, content)
-        print("Downloaded " .. filePath .. " from GitHub.")
+        local currentContent = readfile(filePath)
+        if currentContent ~= content then
+            writefile(filePath, content)
+            print("Downloaded " .. filePath .. " from GitHub.")
+        else
+            print(filePath .. " is already up to date.")
+        end
     else
         warn("Failed to download " .. filePath .. " from GitHub.")
     end
 end
-if not isfolder("Sigma5") then
-    makefolder("Sigma5")
+
+if not isfolder("sigma5") and not isfolder("sigma5/profiles") then
+    makefolder("sigma5")
+    task.wait()
+    makefolder("sigma5/profiles")
 end
 
-GithubUpdateCheck(BedwarsGithub, "Sigma5/Bedwars.lua")
-wait(1)
-print("Debug Logs")
-if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
-    print("Loaded Bedwars.lua")
-    loadfile("Sigma5/Bedwars.lua")()
+GithubUpdateCheck(SkywarsGithub, "Sigma5/Skywars.lua")
+if game.PlaceId == 8542275097 or game.PlaceId == 8768229691 then
+    print("Loaded Skywars.lua...")
+    loadfile("profiles/Skywars.lua")()
 end
