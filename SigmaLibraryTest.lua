0c0c402481567cd21a3478639cd8fe517f76f018
NotHm_ <127741557+AfgMS@users.noreply.github.com>
2024-03-01 17:30:30 +0700

    Create SigmaLibraryTest.lua

---
+++ SigmaLibraryTest.lua
@@ -0,0 +1,7 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/Sigma5LibraryRework.lua", true))()
+
+Library:CreateCore()
+
+local TestOne = Library:CreateTabs("Abror")
+local TestTwo = Library:CreateTabs("Ebrer")
+local TestThree = Library:CreateTabs("Bebek")
