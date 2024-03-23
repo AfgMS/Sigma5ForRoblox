76eb7c6744763109174e8978ec25c77dcd932100
NotHm_ <127741557+AfgMS@users.noreply.github.com>
2024-03-23 19:53:08 +0700

    Update UniversalTest.lua

--- UniversalTest.lua
+++ UniversalTest.lua
@@ -1,10 +1,10 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/NewUITest.lua", true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/NewUITest.lua", true))()
 
-Library:CreateCore()
-wait(1)
 CreateNotification("Universal", "sigma5 successfully loaded", 3, true)
+task.wait()
+lib:CreateCore(true)
 
-local Tab1 = Library:CreateTab("S")
+local Tab1 = lib:CreateTab("S")
 local Buttons1 = Tab1:CreateToggle("Sigmeme", "Sigma reworked", false, function(callback)
     if callback then
         print("Yessir")
@@ -13,7 +13,7 @@ local Buttons1 = Tab1:CreateToggle("Sigmeme", "Sigma reworked", false, function(
     end
 end)
 
-local Tab2 = Library:CreateTab("i")
-local Tab3 = Library:CreateTab("g")
-local Tab4 = Library:CreateTab("m")
-local Tab5 = Library:CreateTab("a")
+local Tab2 = lib:CreateTab("i")
+local Tab3 = lib:CreateTab("g")
+local Tab4 = lib:CreateTab("m")
+local Tab5 = lib:CreateTab("a")
