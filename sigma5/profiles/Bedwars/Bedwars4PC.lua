019f0dad352a36efcf02db52c4399060a53714f6
NotHm_ <127741557+AfgMS@users.noreply.github.com>
2024-04-21 13:08:00 +0700

    Update Bedwars4PC.lua

--- sigma5/profiles/Bedwars/Bedwars4PC.lua
+++ sigma5/profiles/Bedwars/Bedwars4PC.lua
@@ -52,7 +52,7 @@ local function GetBed(range)
 	local localPlayer = game.Players.LocalPlayer
 
 	for _, v in pairs(game.Workspace:GetChildren()) do
-		if v.Name == "bed" and v.Covers.BrickColor ~= localPlayer.Team.TeamColor then
+		if v.Name == "bed" and v.Blanket.BrickColor ~= localPlayer.Team.TeamColor then
 			local distance = (v.Position - localPlayer.Character.HumanoidRootPart.Position).magnitude
 			if distance < nearestDistance and distance <= range then
 				nearestBed = v
@@ -442,9 +442,31 @@ local Fly = PlayerTab:CreateToggle({
     callback = function(enabled)
         if enabled then
             game.Workspace.Gravity = 0
-            wait(1.8)
-            CreateNotification("Fly", "Its about to get flagged", 3, true)
         else
+            game.Workspace.Gravity = 192.6
+        end
+    end
+})
+--LongJump
+local LongJump = PlayerTab:CreateToggle({
+    Name = "LongJump",
+    Bind = "nil",
+    callback = function(enabled)
+        if enabled then
+            task.wait(0.1)
+            localPlayer.Character:FindFirstChild("Humanoid").WalkSpeed = 13
+            game.Workspace.Gravity = 23
+            task.wait()
+            localPlayer.Character:FindFirstChild("Humanoid"):ChangeState(Enum.HumanoidStateType.Jumping)
+            task.wait(0.48)
+            localPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = localPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,-0.2,-2.1)
+            wait(0.5)
+            localPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = localPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,-0.5,-2.1)
+            wait(0.5)
+            localPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = localPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0, 0.2 ,0)
+            wait(0.5)
+        else
+            localPlayer.Character:FindFirstChild("Humanoid").WalkSpeed = 16
             game.Workspace.Gravity = 192.6
         end
     end
