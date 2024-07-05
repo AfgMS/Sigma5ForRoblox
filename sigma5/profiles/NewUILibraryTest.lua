267438bdbd68ba71dddd9c1e035742999cb8fa54
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-07-05 14:36:38 +0700

    Update NewUILibraryTest.lua

--- sigma5/profiles/NewUILibraryTest.lua
+++ sigma5/profiles/NewUILibraryTest.lua
@@ -1,3 +1,10 @@
 --BridgeDuels have some interesting AntiCheat..
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/sigma5/BetaLibrary.lua", true))()
 Library:CreateCore()
+
+Library:CreateTab("Combat")
+Library:CreateTab("Movement")
+Library:CreateTab("Player")
+Library:CreateTab("Render")
+Library:CreateTab("Exploit")
+Library:CreateTab("Misc")
