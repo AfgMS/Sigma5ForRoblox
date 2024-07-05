817b168a1bca0dd4ddc08e4510f4631171dc0a14
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-07-05 15:26:19 +0700

    Update NewUILibraryTest.lua

--- sigma5/profiles/NewUILibraryTest.lua
+++ sigma5/profiles/NewUILibraryTest.lua
@@ -1,11 +1,11 @@
---BridgeDuels have some interesting AntiCheat..
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/sigma5/BetaLibrary.lua", true))()
+local Library = require(game:GetService("ReplicatedStorage"):WaitForChild("Roblox"):WaitForChild("New"):FindFirstChild("Eternal"))
 Library:CreateCore()
 
 local Combat = Library:CreateTab("Combat")
 local Movement = Library:CreateTab("Movement")
+local Player = Library:CreateTab("Player")
 
-local Test = Combat:CrateToggle("Test1", false, true, function(callback)
+local Test = Combat:CrateToggle("Test1", false, false, function(callback)
 	if callback then
 		print("Debug Test1")
 	else
@@ -21,18 +21,26 @@ local Test2 = Combat:CrateToggle("Test2", false, true, function(callback)
 	end
 end)
 
-local Tes3 = Movement:CrateToggle("Test3", false, true, function(callback)
+local Tes3 = Movement:CrateToggle("Test3", false, false, function(callback)
 	if callback then
 		print("Debug 32")
 	else
 		print("Unbug 23")
 	end
 end)
---[[
-local Uninject = Movement:CrateToggle("Uninject", false, false, function(callback)
+
+local Tes4 = Movement:CrateToggle("Test4", false, false, function(callback)
 	if callback then
-		Library.Uninjected = true
-		shared.UninjectEternal()
+		print("Debug 42")
+	else
+		print("Unbug 24")
 	end
 end)
---]]
+
+local Tes4 = Player:CrateToggle("Speed", false, false, function(callback)
+	if callback then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 85
+	else
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 23
+	end
+end)
