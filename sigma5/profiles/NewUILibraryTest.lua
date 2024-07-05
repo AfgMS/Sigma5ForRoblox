5a41a261b298c5e56f5e133012314db354dd9200
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-07-05 17:14:06 +0700

    Update NewUILibraryTest.lua

--- sigma5/profiles/NewUILibraryTest.lua
+++ sigma5/profiles/NewUILibraryTest.lua
@@ -1,6 +1,5 @@
 --BridgeDuels have some interesting AntiCheat..
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AfgMS/Sigma5ForRoblox/main/sigma5/BetaLibrary.lua", true))()
-local Library = require(game:GetService("ReplicatedStorage"):WaitForChild("Roblox"):WaitForChild("New"):FindFirstChild("Eternal"))
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
