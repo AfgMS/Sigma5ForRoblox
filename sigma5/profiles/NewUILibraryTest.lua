9b4d1e3b2950e4123d5d38a7f1ab8319424969ba
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-07-05 15:18:08 +0700

    Update NewUILibraryTest.lua

--- sigma5/profiles/NewUILibraryTest.lua
+++ sigma5/profiles/NewUILibraryTest.lua
@@ -5,14 +5,33 @@ Library:CreateCore()
 local Combat = Library:CreateTab("Combat")
 local Movement = Library:CreateTab("Movement")
 
-local Test = Combat:CrateToggle("Yessir", false, false, function(callback)
-	print("eeee")
+local Test = Combat:CrateToggle("Test1", false, true, function(callback)
+	if callback then
+		print("Debug Test1")
+	else
+		print("Unbug Test1")
+	end
 end)
 
-local Test = Movement:CrateToggle("Yessir", false, true, function(callback)
+local Test2 = Combat:CrateToggle("Test2", false, true, function(callback)
 	if callback then
-		print("e")
+		print("Debug ..")
 	else
-		print("o")
+		print("Unbug --")
+	end
+end)
+
+local Tes3 = Movement:CrateToggle("Test3", false, true, function(callback)
+	if callback then
+		print("Debug 32")
+	else
+		print("Unbug 23")
+	end
+end)
+
+local Uninject = Movement:CrateToggle("Uninject", false, false, function(callback)
+	if callback then
+		Library.Uninjected = true
+		shared.UninjectEternal()
 	end
 end)
